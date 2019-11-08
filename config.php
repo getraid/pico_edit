f2d4edb14426990788cd38819c663981866dafa1
GetRaid <32432718+getraid@users.noreply.github.com>
2019-11-09 00:59:49 +0100

    Added tui.editor (not sure, if implementable )

--- config.php
+++ config.php
@@ -6,6 +6,8 @@ global $backend_password;
  * This should be a sha256 hash of your password.
  * Use a tool like https://convertstring.com/Hash/SHA256 to generate.
  */
+
+ // TODO: 'Dont forget to delete temporary password...' 
 $backend_password = '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B';
 
 ?>
