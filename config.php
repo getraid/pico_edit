51f95b503bf49f24cd9c1ec8bbab0991677fa6a4
GetRaid <32432718+getraid@users.noreply.github.com>
2019-11-08 23:15:18 +0100

    Added BlackrockDigital/startbootstrap-sb-admin

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ global $backend_password;
  * This should be a sha256 hash of your password.
  * Use a tool like https://convertstring.com/Hash/SHA256 to generate.
  */
-$backend_password = '';
+$backend_password = '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B';
 
 ?>
